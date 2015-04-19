c8107a1340b43a2f29c10bd36a4f29bf53cb5295
Vincent Lee <vlee@prescientimaging.com>
2015-04-18 21:45:49 -0700

    deleting files

--- EE_355/EE_355/maze_io.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAZE_IO_H
-#define MAZE_IO_H
-
-char * read_maze(char *filename, int *rows, int *cols );
-void print_maze(char *maze, int rows, int cols);
-void write_maze(char *filename, char *maze, int rows, int cols);
-
-#endif
